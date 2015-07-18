3a3ce617962eb2c3ca6f675bc45fc28181915b25
Jan Kahnt <kahnt.jan@gmail.com>
2015-07-18 23:48:29 +0200

    added gui basic structure

--- src/de/steammanager/StartGui.java
+++ src/de/steammanager/StartGui.java
@@ -1,12 +1,45 @@
 package de.steammanager;
 
+import java.awt.EventQueue;
+
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import de.steammanager.gui.SteamMainWindow;
 
 public class StartGui {
 
 	public static void main(String[] args) {
-		SteamMainWindow mw = new SteamMainWindow();
-
+		
+		try {
+			for(LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()){
+				if("Nimbus".equals(info.getName())){
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new SteamMainWindow().setVisible(true);
+				
+			}
+		});
 	}
-
 }
--- src/de/steammanager/gui/SteamMainWindow.java
+++ src/de/steammanager/gui/SteamMainWindow.java
@@ -4,31 +4,81 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 
 
-public class SteamMainWindow extends JFrame implements ActionListener{
+public class SteamMainWindow extends JFrame{
 	
 	private Container contentPane;
 	private JPanel mainPanel;
+    private JLabel sourceLabel;
+    private JList<String> sourceList;
+    private JScrollPane sourceScrollPane;
+    private JTextField sourceText;
+    private JLabel targetLabel;
+    private JList<String> targetList;
+    private JScrollPane targetScrollPane;
+    private JTextField targetText;
+    private JButton toSourceButton;
+    private JButton toTargetButton;
 	
 	public SteamMainWindow(){
-		this.contentPane = this.getContentPane();
-		this.mainPanel = new JPanel();
+		initComponents();
+	}
+	
+	private void initComponents(){
+		contentPane = this.getContentPane();
+		mainPanel = new JPanel();
 		
-		this.setTitle("SteamManager");
-		this.setVisible(true);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setLocation(10, 10);
-		this.setSize(200, 320);
+		sourceLabel = new JLabel();
+		sourceText = new JTextField();
+		sourceScrollPane = new JScrollPane();
+		sourceList = new JList<String>();
+		
+		targetLabel = new JLabel();
+		targetText = new JTextField();
+		targetScrollPane = new JScrollPane();
+		targetList = new JList<String>();
+		
+		toSourceButton = new JButton("<");
+		toTargetButton = new JButton(">");
+		
+		sourceLabel.setText("Quelle:");		
+		targetLabel.setText("Ziel:");
+		
+		toSourceButton.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				toSourceButtonActionPerformed(event);				
+			}
+		});
+		
+		toTargetButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				toTargetButtonActionPerformed(event);				
+			}
+		});	
+		
+		setTitle("SteamManager");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocation(10, 10);
+		setSize(200, 320);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void toSourceButtonActionPerformed(ActionEvent event){
+		// TODO fill action listener		
+	}
+	
+	public void toTargetButtonActionPerformed(ActionEvent event){
+		// TODO fill action listener
 	}
 
 }
